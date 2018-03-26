573031fe6eac3cfbed7f26ae5310f35f33a0678b
pmaldonado <prmm95@gmail.com>
2018-03-26 17:03:05 -0400

    Se listan los productos disponibles en la vista platform

--- vendedor/ecommerce/sales/views.py
+++ vendedor/ecommerce/sales/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
-from sales.models import Client
+from sales.models import Client, Product
 
 # Create your views here.
 
@@ -10,29 +10,36 @@ def index(request):
     return render(request, 'sales/index.html')
 
 def register(request):
-
-
     if request.method == 'POST':
         try:
             username = request.POST['username']
             password = request.POST['psw']
-
-            #Debe tener una longitud mínima, al menos un carácter no alfa numérico y una letra mayúscula
             user = Client(username=username,password=password)
             user.save()
             return render(request,'sales/index.html')
         except:
-            print("Error registrando al usuario")
-            return HttpResponse("Error")
+            return render(request,'sales/error.html')
 
     else:
         return render(request,'sales/register.html')
 
 def login(request):
-    return render(request,'sales/login.html')
+    if request.method == 'POST':
+        return render(request,'sales/platform.html')
+    else:
+        return render(request,'sales/login.html')
 
 def logout(request):
     return HttpResponse("Logout")
 
 def platform(request):
-    return render(request,'sales/platform.html')
+    if request.method == 'POST':
+        # Process a transaction
+        print("Process a transaction")
+    else:
+
+        products_list = Product.objects.all()
+        context = {
+            'products_list': products_list,
+            }
+        return render(request,'sales/platform.html',context)
