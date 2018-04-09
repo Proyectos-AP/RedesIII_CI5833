959b4b1e094f200fc689c9e3ac920d20972487b3
Fernando Benjamín Pérez Maurera <fperezmaurera@gmail.com>
2018-04-08 20:24:28 -0400

    añadida las referencias a los entrypoints.

--- banco-cliente/entrypoint.sh
+++ banco-cliente/entrypoint.sh
@@ -5,6 +5,8 @@
 # Este script se encarga de correr las migraciones y la ubicación de
 # los archivos son según donde se ejecute el archivo docker-compose.yml
 #
+# Ref: https://stackoverflow.com/a/41436850
+#
 # Autores: Fernando Pérez, Alejandra Cordero y Pablo Maldonado.
 python ./banco-cliente/bancoCliente/manage.py makemigrations
 python ./banco-cliente/bancoCliente/manage.py migrate
--- vendedor/entrypoint.sh
+++ vendedor/entrypoint.sh
@@ -5,6 +5,8 @@
 # Este script se encarga de correr las migraciones y la ubicación de
 # los archivos son según donde se ejecute el archivo docker-compose.yml
 #
+# Ref: https://stackoverflow.com/a/41436850
+#
 # Autores: Fernando Pérez, Alejandra Cordero y Pablo Maldonado.
 python ./vendedor/ecommerce/manage.py makemigrations
 python ./vendedor/ecommerce/manage.py migrate
