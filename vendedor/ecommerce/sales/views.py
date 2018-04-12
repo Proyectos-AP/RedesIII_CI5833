19b453f8e2fd99f133aa74f80d43df92e65dc295
Fernando Benjamín Pérez Maurera <fperezmaurera@gmail.com>
2018-04-11 22:25:45 -0400

    Añadir búsqueda de facturas para el usuario y bloqueo/desbloqueo del usuario si se equivoca más de tres veces.

--- vendedor/ecommerce/sales/views.py
+++ vendedor/ecommerce/sales/views.py
@@ -6,11 +6,12 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
-from sales.models import Product
+from sales.models import Product, Receipt
 import requests
 import json
 
 URL_BANCO_CLIENTE = "http://127.0.0.1:8080/"
+USERS_ATTEMPT = dict()
 # Create your views here.
 def index(request):
 
@@ -51,10 +52,15 @@ def login_view(request):
         json_data = json.loads(response.text)
 
         if json_data['success']:
-            # Verify user credentials
+            # Verify user credentials.
+            # First take into account if the user is locked.
+            if (username in USERS_ATTEMPT.keys() and USERS_ATTEMPT[username] > 3):
+                return render(request,'sales/locked.html')
+
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
+                USERS_ATTEMPT[username] = 0
                 # Redirect to sales platform
                 products_list = Product.objects.all()
                 print("Productos", products_list)
@@ -64,6 +70,12 @@ def login_view(request):
                     }
                 return render(request,'sales/platform.html',context)
             else:
+                # Authentication failhere, we must count
+                if username in USERS_ATTEMPT.keys():
+                    USERS_ATTEMPT[username] += 1
+                else:
+                    USERS_ATTEMPT[username] = 1
+
                 return render(request,'sales/login.html')
         else:
             return render(request,'sales/login.html')
@@ -84,6 +96,7 @@ def platform(request):
         print("Process a transaction")
     else:
         products_list = Product.objects.all()
+        previous_purchases = Receipt.objects.filter(client=request.user)
         print("Productos", products_list)
         context = {
             'products_list': products_list,
@@ -98,6 +111,7 @@ def unlock(request):
     if request.method == 'POST':
         username = request.POST['username']
         reset(username=username)
+        USERS_ATTEMPT[username] = 0
         return(render(request,'sales/index.html'))
 
     else:
